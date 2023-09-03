56277d5e16f06ab3733bbd2cebec2ac528ae550c
Jake2k4 <119366047+jakeyboi1@users.noreply.github.com>
2023-09-03 11:05:59 -0700

    Add files via upload

---
+++ sourceCode/stdafx.h
@@ -0,0 +1,10 @@
+#pragma once
+#include <iostream>
+#include <Windows.h>
+#include<TlHelp32.h>
+#include <psapi.h> // For EnumProcessModules
+#include <tchar.h> // _tcscmp
+#include <vector>
+#include "helperFuncts.hpp"
+#include "MemoryHacks.hpp"
+#include "callingGameFuncts.hpp"
